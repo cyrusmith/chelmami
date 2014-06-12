4f6d584ac66008332ba1fa85c33c6bdf40facab5
cyrusmith <info@interosite.ru>
2014-06-12 20:25:45 +0600

    + filtering

--- wp-content/plugins/intero-filter/intero-filter.php
+++ wp-content/plugins/intero-filter/intero-filter.php
@@ -10,29 +10,157 @@ Text Domain: interofilter
 License: GPLv2
 */
 
-function intrfltr_init() {
-
-    register_post_type( 'afisha',
-        array(
-            'labels' => array(
-                'name' => __( 'Afishas' ),
-                'singular_name' => __( 'Afisha' )
-            ),
-            'public' => true,
-            'has_archive' => true,
-        )
-    );
-
-    register_taxonomy(
-        'afisha_city',
-        'afisha',
-        array(
-            'label' => __( 'Afisha city' ),
-            'rewrite' => array( 'slug' => 'afisha_city' )
-        )
-    );
-
+function interoFilter_outputTextField($field, $value = null)
+{
+    ?>
+    <input type="text" name="interofilter_<?php echo $field['id'] ?>"
+           value="<?php echo !empty($value) ? $value : '' ?>"/>
+<?php
 }
 
+function interoFilter_outputDateField($field, $value = null)
+{
+    ?>
+    <input type="text" class="datetime" name="interofilter_<?php echo $field['id'] ?>"
+           value="<?php echo !empty($value) ? $value : '' ?>"/>
+<?php
+}
 
-add_action('init', 'intrfltr_init');
+function interoFilter_outputNumberField($field, $value = null)
+{
+    ?>
+    <input type="text" class="numeric" name="interofilter_<?php echo $field['id'] ?>"
+           value="<?php echo !empty($value) ? $value : '' ?>"/>
+
+<?php
+}
+
+function interoFilter_outputSelectField($field, $value = null)
+{
+    ?>
+    <select name="interofilter_<?php echo $field['id'] ?>">
+        <?php
+        foreach ($field['data']['options'] as $optKey => $optData) {
+            if (!is_array($optData)) continue;
+            ?>
+            <option value="<?php echo $optData['value'] ?>"><?php echo $optData['title']?></option>
+        <?php
+        }
+        ?>
+    </select>
+<?php
+}
+
+function interoFilter_outputForm($args)
+{
+
+    if (empty($args->query_vars['post_type']) or $args->query_vars['post_type'] != 'afisha') {
+        return;
+    }
+
+    $fields = get_option('wpcf-fields');
+
+    ?>
+    <form action="" method="post" class="interofilter-form">
+        <?php
+        foreach ($fields as $key => $field) {
+            ?>
+            <div class="input">
+                <label for="interofilter_<?php echo $field['id'] ?>"><?php echo $field['name'] ?>:</label>
+                <?php
+                switch ($field['type']) {
+                    case 'select':
+                        interoFilter_outputSelectField($field);
+                        break;
+                    case 'date':
+                        interoFilter_outputDateField($field);
+                        break;
+                    case 'numeric':
+                        interoFilter_outputNumberField($field);
+                        break;
+                    default:
+                        interoFilter_outputTextField($field);
+                }
+                ?>
+            </div>
+        <?php
+        }
+        ?>
+        <div class="input submit">
+            <button type="submit">
+                Найти
+            </button>
+        </div>
+    </form>
+<?php
+}
+
+function interoFilter_searchFilter($query)
+{
+
+    $fields = get_option('wpcf-fields');
+
+    if (!empty($query->query) && !empty($query->query['post_type']) && $query->query['post_type'] == 'afisha') {
+
+        if (!is_admin() /*&& $query->is_main_query()*/) {
+            $metaQuery = array();
+            foreach ($query->query_vars as $key => $value) {
+                if (strpos($key, "interofilter_") === 0) {
+
+                    $fieldId = str_replace("interofilter_", "", $key);
+                    $fieldVal = $value;
+
+                    $query->set("interofilter_" . $fieldId, "");
+
+                    if (!array_key_exists($fieldId, $fields)) {
+                        continue;
+                    }
+
+                    $compare = "LIKE";
+
+                    if ($fields[$fieldId]['type'] == "date") {
+                        $dateInfo = date_parse($value);
+                        if (!empty($dateInfo['errors'])) {
+                            continue;
+                        }
+                        $timeStamp = mktime(0, 0, 0, $dateInfo['month'], $dateInfo['day'], $dateInfo['year']);
+                        $fieldVal = $timeStamp . "";
+                    }
+
+                    $metaQuery[] = array(
+                        'key' => "wpcf-" . $fieldId,
+                        'value' => $fieldVal,
+                        'compare' => $compare
+                    );
+                }
+            }
+            $metaQuery['relation'] = 'AND';
+
+            $query->set('meta_query', $metaQuery);
+
+            if ($query->is_search) {
+                //TODO
+            }
+        }
+    }
+    return $query;
+}
+
+function interoFilter_addQueryvars($qvars)
+{
+    $fields = get_option('wpcf-fields');
+    foreach ($fields as $key => $field) {
+        $qvars[] = "interofilter_" . $key;
+    }
+    return $qvars;
+}
+
+add_action('pre_get_posts', 'interoFilter_searchFilter');
+
+add_action('loop_start', 'interoFilter_outputForm');
+
+add_filter('query_vars', 'interoFilter_addQueryvars');
+
+wp_enqueue_style('intero-filter-style', plugins_url('jquery-ui-1.10.4.custom.min.css', __FILE__));
+wp_enqueue_script('jquery-ui-datepicker');
+wp_enqueue_script('intero-filter-script', plugins_url('script.js', __FILE__));
